a82615e61e7fb708693eb912a09ee776622ce7dd
Wayne Tong <dkotrzarashi@gmail.com>
2022-06-06 15:05:13 -0400

    Change component from functions to classes

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,13 @@
-function Footer() {
-    return (
-    <div>
-        <h1>Footer here</h1>
-    </div>
-    );
+import React from "react";
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Footer here</h1>
+            </div>
+        );
+    }
 }
   
 export default Footer;
