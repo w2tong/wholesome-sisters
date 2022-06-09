e6638daa23ec3b66e896206529a1183ff8495529
Wayne Tong <dkotrzarashi@gmail.com>
2022-06-08 23:04:13 -0400

    Add links to RaiderIO and WarcraftLogs in Member

--- src/RosterList/Member.tsx
+++ src/RosterList/Member.tsx
@@ -2,28 +2,36 @@ import useCollapse from 'react-collapsed'
 import styled from 'styled-components'
 
 const Content = styled.section`
-border: 5px solid red;
-overflow: auto;
+
+`
+const Container = styled.div`
+border: 1px solid black;
 `
 
 interface MemberProps {
   name: string
 }
 
+const raiderIOLink = 'https://raider.io/characters/us/bleeding-hollow/'
+const raiderIOCharacterWidgetSettings = '?embed=1&embedmode=summary&embedname=1&classcolors=1&characterBackground=1&showtime=10&chromargb=transparent'
+
 function Member (props: MemberProps) {
   const { getCollapseProps, getToggleProps, isExpanded } = useCollapse()
-  const raiderIOLink = `https://raider.io/characters/us/bleeding-hollow/${props.name}`
-
+  // TODO: dynamically change width and height of RaiderIO widget
   return (
-    <div>
+    <Container>
       <div {...getToggleProps()}>
         {props.name} {isExpanded ? '-' : '+'}
       </div>
       <Content {...getCollapseProps()}>
-        <a href={raiderIOLink} target="_blank" rel="noreferrer">link</a>
-        <iframe src={`${raiderIOLink}?embed=1&embedmode=summary&embedname=1&classcolors=1&characterBackground=1&showtime=10&chromargb=transparent`} width="805px" height="181px" />
+        <a href={raiderIOLink + props.name} target="_blank" rel="noreferrer">
+          <img title="View Raider.IO Page" src="https://assets.rpglogs.com/img/warcraft/raiderio_square_xl.png" alt="View Raider.IO Page"/>
+        </a>
+        <a href="https://www.warcraftlogs.com/character/us/bleeding-hollow/Fourlegs" title="Warcraft Logs Profile" target="_blank" rel="noopener noreferrer">wclogs</a>
+
+        <iframe src={raiderIOLink + props.name + raiderIOCharacterWidgetSettings} width="805px" height="181px" />
       </Content>
-    </div>
+    </Container>
   )
 }
 
