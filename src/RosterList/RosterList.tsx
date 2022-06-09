11dca795d1b9aa3b935d8e5905e64cdd36c0b669
Wayne Tong <dkotrzarashi@gmail.com>
2022-06-08 23:18:00 -0400

    Add SearchBar to RosterList

--- src/RosterList/RosterList.tsx
+++ src/RosterList/RosterList.tsx
@@ -1,12 +1,17 @@
+import { useState } from 'react'
+import SearchBar from '../SearchBar'
 import Member from './Member'
 import rosterJSON from './roster.json'
 
 function RosterList () {
-  const roster = rosterJSON.map(member => <Member {...member} key={member.name}/>)
+  const [input, setInput] = useState('')
+  const roster = rosterJSON.filter(member => member.name.toLowerCase().includes(input.trim().toLowerCase())
+  ).map(member => <Member {...member} key={member.name}/>)
 
   return (
     <div className="memberWindow overflow-auto">
       <h1>Roster list here</h1>
+      <SearchBar handleChange={setInput} value={input}/>
       {roster}
     </div>
   )
