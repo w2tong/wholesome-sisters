8c7640fb69c5071954be0eeacdfb8c673be5d56f
Wayne Tong <dkotrzarashi@gmail.com>
2022-06-14 05:24:55 -0400

    Remove whitespace

--- src/IFrame.tsx
+++ src/IFrame.tsx
@@ -16,7 +16,7 @@ const Spinner = styled.div`
   border: 16px solid #f3f3f3; /* Light grey */
   border-top: 16px solid #3498db; /* Blue */
   border-radius: 50%;
-  animation: ${spinAnimation} 2s linear infinite; 
+  animation: ${spinAnimation} 2s linear infinite;
 `
 
 const SpinnerContainer = styled.div`
