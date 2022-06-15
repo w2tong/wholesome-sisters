5617796c3dea4504c0e6e9b891c9c298c31b28eb
Wayne Tong <dkotrzarashi@gmail.com>
2022-06-15 00:56:10 -0400

    Adjust padding

--- src/GuildInfo.tsx
+++ src/GuildInfo.tsx
@@ -8,18 +8,23 @@ const FlexboxContainer = styled.div`
 
 `
 
+const Container = styled.div`
+  border: white solid 1px;
+  padding-bottom: 25px;
+`
+
 const BossProgressURL = 'https://raider.io/widgets/boss-progress?raid=sepulcher-of-the-first-ones&difficulty=heroic&region=us&realm=bleeding-hollow&guild=Wholesome+Sisters&boss=latest&period=until_kill&hide=&chromargb=transparent'
 // const GearCompositionURL = 'https://raider.io/widgets/guild-gear-composition?raid=sepulcher-of-the-first-ones&min_tier_tokens=1&region=us&realm=bleeding-hollow&guild=Wholesome+Sisters&scope=role&tier_placement=column&chromargb=transparent'
 
 function GuildInfo () {
   return (
-    <div style={{ border: 'white solid 1px' }}>
+    <Container>
       <h1>Guild Info here</h1>
       <FlexboxContainer>
         <IFrame url={BossProgressURL} width={400} height={180}/>
         {/* <IFrame src={GearCompositionURL}/> */}
       </FlexboxContainer>
-    </div>
+    </Container>
   )
 }
 
