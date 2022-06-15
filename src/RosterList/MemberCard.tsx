6794132bdd79329294901164a3432928d336e0e0
Wayne Tong <dkotrzarashi@gmail.com>
2022-06-15 02:52:01 -0400

    Replace iframes from MemberCard with static content

--- src/RosterList/MemberCard.tsx
+++ src/RosterList/MemberCard.tsx
@@ -1,47 +1,26 @@
 import styled from 'styled-components'
-import IFrame from '../IFrame'
 import HorizontalRule from '../HorizontalRule'
 import { BackgroundColor } from '../styles'
-import media from '../media'
 
 const warcraftLogsLink = 'https://www.warcraftlogs.com/character/us/bleeding-hollow/'
 const raiderIOLink = 'https://raider.io/characters/us/bleeding-hollow/'
-const raiderIOCharacterWidgetSettings = '?embed=1&embedmode=summary&embedname=1&classcolors=1&characterBackground=1&showtime=10&chromargb=transparent'
 
 const Container = styled.div`
   border-radius: 10px;
   padding: 25px;
   margin: 10px;
   background-color: ${BackgroundColor};
+  width: 320px;
+  height: 250px;
 `
 
 const GameContainer = styled.div`
   margin: 5px 0px;
 `
 
-const raiderIOCharWidgetWidth = 805
-const raiderIOCharWidgetHeight = 181
-const raiderIOCharWidgetScale = 0.6
-const tabletScale = 0.4
-const mobileScale = 0.18
-const IFrameContainer = styled.div`
-  width: ${raiderIOCharWidgetWidth * raiderIOCharWidgetScale}px;
-  height: ${raiderIOCharWidgetHeight * raiderIOCharWidgetScale}px;
-  transform: scale(${raiderIOCharWidgetScale});
-  transform-origin: 0 0;
-  @media (max-width: ${media.Tablet}px) {
-    transform: scale(${tabletScale});
-    width: ${raiderIOCharWidgetWidth * tabletScale}px;
-    height: ${raiderIOCharWidgetHeight * tabletScale}px;
-  }
-  @media (max-width: ${media.Mobile}px) {
-    transform: scale(${mobileScale});
-    width: ${raiderIOCharWidgetWidth * mobileScale}px;
-    height: ${raiderIOCharWidgetHeight * mobileScale}px;
-  }
-`
-
 const Name = styled.div`
+  display: flex;
+  justify-content: space-between;
   font-size: 16px;
 `
 
@@ -53,31 +32,33 @@ const Role = styled.span`
 interface MemberProps {
   name: string;
   role: string;
-  wowName: string;
+  wow: {
+    name: string;
+    class: string;
+    portraitURL: string;
+  }
   lostArk: {
     name: string;
     class: string;
-    itemLevel: number;
   } | null;
 }
 
 function Member (props: MemberProps) {
   return (
       <Container>
-        <Name>{props.name} <Role>{props.role}</Role></Name>
+        <Name>{props.name}<Role>{props.role}</Role></Name>
         <HorizontalRule text="World of Warcraft" fontSize={14} lineHeight={2}/>
         <GameContainer>
-          <a href={raiderIOLink + props.wowName} target="_blank" rel="noreferrer">
+          <img src={props.wow.portraitURL}/>
+          <div>{`${props.wow.name} ${props.wow.class}`}</div>
+          <a href={raiderIOLink + props.wow.name} target="_blank" rel="noreferrer">
             <img title="View Raider.IO Page" src="https://assets.rpglogs.com/img/warcraft/raiderio_square_xl.png" alt="View Raider.IO Page"/>
           </a>
-          <a href={warcraftLogsLink + props.wowName} title="Warcraft Logs Profile" target="_blank" rel="noopener noreferrer"><img title="View Warcraft Logs Page" src="https://assets.rpglogs.com/img/warcraft/favicon.png" alt="View Warcraft Logs Page"/></a>
-          <IFrameContainer>
-            <IFrame url={raiderIOLink + props.wowName + raiderIOCharacterWidgetSettings} width={raiderIOCharWidgetWidth} height={raiderIOCharWidgetHeight}/>
-          </IFrameContainer>
+          <a href={warcraftLogsLink + props.wow.name} title="Warcraft Logs Profile" target="_blank" rel="noopener noreferrer"><img title="View Warcraft Logs Page" src="https://assets.rpglogs.com/img/warcraft/favicon.png" alt="View Warcraft Logs Page"/></a>
         </GameContainer>
         <HorizontalRule text="Lost Ark" fontSize={14} lineHeight={2}/>
         <GameContainer>
-          {props.lostArk ? `${props.lostArk.name} ${props.lostArk.class} ${props.lostArk.itemLevel}` : <div>Donezo</div>}
+          {props.lostArk ? `${props.lostArk.name} ${props.lostArk.class}` : <div>Donezo</div>}
         </GameContainer>
       </Container>
   )
