22d6c2cc7eb09219d63130de74e99118e591b4ca
Wayne Tong <dkotrzarashi@gmail.com>
2022-06-16 03:45:48 -0400

    Update padding

--- src/LatestNews/LatestNews.tsx
+++ src/LatestNews/LatestNews.tsx
@@ -1,15 +1,20 @@
+import styled from 'styled-components'
 import NewsCard from './NewsCard'
 import articlesJSON from '../json/articles.json'
-import { Title, Body } from '../styles'
+import { Title } from '../styles'
+
+const Container = styled.div`
+  padding: 0 2.5vw;
+`
 
 function NewsFeed () {
   const articles = articlesJSON.slice(0, 3).map(article => <NewsCard {...article} key={article.title}/>)
 
   return (
-    <Body>
+    <Container>
       <Title>Latest News</Title>
       {articles}
-    </Body>
+    </Container>
   )
 }
 
