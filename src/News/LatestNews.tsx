2e50b3a736d58be616b7b7ff35b4a0b7bb5ced4f
Wayne Tong <dkotrzarashi@gmail.com>
2022-06-19 05:37:57 -0400

    Add className to container to allow for styling

--- src/News/LatestNews.tsx
+++ src/News/LatestNews.tsx
@@ -1,9 +1,14 @@
+import styled from 'styled-components'
 import NewsCard from './NewsCard'
 import articlesJSON from '../json/articles.json'
 import { Title } from '../styles'
 
+const StyledNewsCard = styled(NewsCard)`
+  margin-bottom: 10px;
+`
+
 function NewsFeed ({ className } : {className?: string}) {
-  const articles = articlesJSON.slice(0, 3).map(article => <NewsCard {...article} key={article.title}/>)
+  const articles = articlesJSON.slice(0, 3).map(article => <StyledNewsCard {...article} key={article.title}/>)
 
   return (
     <div className={className}>
