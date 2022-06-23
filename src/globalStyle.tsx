eb7080c1d53eab828a0eda5fe187c83fbf4c5dd0
Wayne Tong <dkotrzarashi@gmail.com>
2022-06-23 02:57:02 -0400

    Move App.css to globalStyle.tsx using styled-components

---
+++ src/globalStyle.tsx
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    height: 100%;
+    background-color: rgb(16, 16, 16);
+    color: white;
+    margin: 0;
+    font-family: "Open Sans", Arial, "Helvetica Neue", Helvetica, sans-serif;
+  }
+
+  #root {
+    height: 100%;
+  }
+`
+
+export default GlobalStyle
