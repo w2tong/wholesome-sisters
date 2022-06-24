8904ea2817d7df14889ad5a491a6eff30897795e
Wayne Tong <dkotrzarashi@gmail.com>
2022-06-24 06:26:16 -0400

    Add image modal to NewsArticle

---
+++ src/News/ImageModal.tsx
@@ -0,0 +1,72 @@
+import { useState, useEffect } from 'react'
+import styled from 'styled-components'
+
+interface ImageModalProps {
+  src: string;
+  className?: string;
+}
+
+const Thumbnail = styled.img`
+  width: 100%;
+  max-width: 500px;
+  max-height: 500px;
+  height: auto;
+  cursor: pointer;
+  &:hover {
+    filter: brightness(1.6);
+  }
+`
+
+const Modal = styled.div<{isOpen: boolean}>`
+  display: ${props => props.isOpen ? 'flex' : 'none'};
+  position: fixed; /* Stay in place */
+  z-index: 2; /* Sit on top */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgba(0,0,0,0.8);
+`
+
+const CloseButton = styled.p`
+  position: absolute;
+  top: 0;
+  right: 15px;
+  cursor: pointer;
+  font-size: 64px;
+  font-weight: bold;
+  margin: 0;
+`
+
+const ModalImage = styled.img`
+  margin: auto;
+  max-width: 90%;
+  max-height: 100%;
+`
+
+function ImageModal (props: ImageModalProps) {
+  const [isOpen, setIsOpen] = useState(false)
+
+  useEffect(() => {
+    const close = (e: {key: string}) => {
+      if (e.key === 'Escape') {
+        setIsOpen(false)
+      }
+    }
+    window.addEventListener('keydown', close)
+    return () => window.removeEventListener('keydown', close)
+  }, [])
+
+  return (
+    <div className={props.className}>
+      <Thumbnail src={props.src} onClick={() => setIsOpen(true)}/>
+      <Modal isOpen={isOpen} onClick={() => setIsOpen(false)}>
+        <CloseButton onClick={() => setIsOpen(false)}>&times;</CloseButton>
+        <ModalImage src={props.src} onClick={e => e.stopPropagation()}/>
+      </Modal>
+    </div>
+  )
+}
+
+export default ImageModal
--- src/News/NewsArticle.tsx
+++ src/News/NewsArticle.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import CategoryTag from '../CategoryTag'
+import ImageModal from './ImageModal'
 import { Header, Date, Text, ContentBackgroundColor } from '../styles'
 
 interface ArticleProps {
@@ -43,7 +44,14 @@ const StyledText = styled(Text)`
   padding: ${padding};
 `
 
+const StyledImageModal = styled(ImageModal)`
+  display: flex;
+  justify-content: center;
+`
+
 function NewsArticle (props: ArticleProps) {
+  const image = require(`../img/${props.imgFile}`)
+
   return (
     <Container className={props.className}>
       <HeaderContainer>
@@ -51,8 +59,9 @@ function NewsArticle (props: ArticleProps) {
         <StyledCategoryTag category={props.category}/>
         <Date>{props.date}</Date>
       </HeaderContainer>
-      {props.imgFile && props.imgAlt && <Image src={require(`../img/${props.imgFile}`)} alt={props.imgAlt} />}
+      {props.imgFile && props.imgAlt && <Image src={image} alt={props.imgAlt} />}
       <StyledText>{props.text}</StyledText>
+      <StyledImageModal src={image}/>
     </Container>
   )
 }
