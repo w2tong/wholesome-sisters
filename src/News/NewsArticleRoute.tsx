c4acf6322eced04762b53e1b616bd2ce2ce0de3f
Wayne Tong <dkotrzarashi@gmail.com>
2022-06-28 00:57:18 -0400

    Fix news articles not being centered

--- src/News/NewsArticleRoute.tsx
+++ src/News/NewsArticleRoute.tsx
@@ -2,8 +2,11 @@ import { useParams } from 'react-router-dom'
 import NewsArticle from './NewsArticle'
 import articlesJSON from '../json/articles.json'
 import styled from 'styled-components'
+import media from '../media'
 
 const Container = styled.div`
+  margin: 0 auto;
+  max-width: ${media.Large}px;
   padding: 10px;
 `
 
